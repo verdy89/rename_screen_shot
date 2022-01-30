0d027f2f97626ff6734d4fe7cc7a999d9b47257e
Ryo Ito <ryo.ito@livesense.co.jp>
2022-01-30 18:43:59 +0900

    不要なオプションを削除

--- rename_screen_shot.sh
+++ rename_screen_shot.sh
@@ -11,7 +11,7 @@ fi
 fswatch -0 $TARGET_DIR | while read -d "" input_path; do
   if [[ ! ${input_path} =~ ^$TARGET_DIR/\. ]]; then
     from=${input_path}
-    to=$(echo ${input_path} | LC_COLLATE=C.UTF-8 sed -e s:"/${DEFAULT_PREFIX} ":/:g | sed -e s:" ":_:g)
+    to=$(echo ${input_path} | sed -e s:"/${DEFAULT_PREFIX} ":/:g | sed -e s:" ":_:g)
 
     # ファイルが削除される時も検出されてしまって mv の対象がなくなるので、エラー出力は捨てる
     mv -n "${from}" "${to}" 2>/dev/null
